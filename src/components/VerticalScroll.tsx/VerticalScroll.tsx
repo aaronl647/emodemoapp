4ab78922ad9f132ff97e04f32b7c1352c8397ea3
alei647 <alei@wagepoint.com>
2022-03-24 15:27:00 -0400

    removed unnecessary files

---
+++ src/components/VerticalScroll.tsx/VerticalScroll.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import "./VerticalScroll.css";
+
+function VerticalScroll() {
+  const constraintRef = useRef(null);
+
+  // const handleChange = () => {
+  //   console.log(e);
+  // };
+  return (
+    <div className="body">
+      <motion.div className="container" ref={constraintRef}>
+        <motion.div
+          className="item"
+          drag
+          dragConstraints={constraintRef}
+          onDrag={(event, info) => {
+            console.log(event);
+          }}
+        />
+      </motion.div>
+    </div>
+  );
+}
+
+export default VerticalScroll;
