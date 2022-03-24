379444a0bb7dee48003c1b04bd73d01441b38f86
aaronl647 <aaron.l@live.ca>
2022-03-24 15:44:59 -0400

    first commit

--- src/components/VerticalScroll.tsx/VerticalScroll.tsx
+++ src/components/VerticalScroll.tsx/VerticalScroll.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useRef } from "react";
 import { motion } from "framer-motion";
 import "./VerticalScroll.css";
-
+//
 function VerticalScroll() {
   const constraintRef = useRef(null);
 
